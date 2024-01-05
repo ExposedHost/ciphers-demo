55d550478c66b51a300c948cc25655057d19200b
ExposedHost <b20245@students.iitmandi.ac.in>
2024-01-05 20:49:20 +0530

    Skeleton code structure

--- src/main/java/com/ExposedHost/Main.java
+++ src/main/java/com/ExposedHost/Main.java
@@ -1,7 +1,10 @@
 package com.ExposedHost;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("The Input format is: \n1. Cipher type \n2. Encrypt or Decrypt \n3. key \n4. The text to en/decrypt");
+        //TODO: A CLI with good user interface
     }
 }
